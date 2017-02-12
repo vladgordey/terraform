af8725e03fe1100445a1f8bbe94cc95fb789960d
vladgordey <vladgordey@gmail.com>
2017-02-11 21:13:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 ### Network
 
-data "aws_availability_zones" "available" {}
+#### data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.10.0.0/16"
