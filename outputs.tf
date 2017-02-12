da7c2f3d7e0c050aa3650be0cf552476b6e2f8ab
vladgordey <vladgordey@gmail.com>
2017-02-11 20:47:40 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,6 @@ output "instance_security_group" {
   value = "${aws_security_group.instance_sg.id}"
 }
 
-output "launch_configuration" {
-  value = "${aws_launch_configuration.app.id}"
-}
 
 output "asg_name" {
   value = "${aws_autoscaling_group.app.id}"
