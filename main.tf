6f1a8b34468ccf3b23395a701c6305e3afa86da7
vladgordey <vladgordey@gmail.com>
2017-02-11 21:14:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   count             = "${var.az_count}"
   cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
 }
 
