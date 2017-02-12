6257bb8448b561acbfa65bbc85bf2ce92d35d9fd
vladgordey <vladgordey@gmail.com>
2017-02-11 20:41:30 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,10 @@ variable "aws_region" {
 }
 variable "short_name" {default = "gordey"}
 
+variable "vpc_cidr" {default = "10.0.0.0/16"}
+variable "private_subnet_cidr" {default = "10.0.2.0/24"}
+variable "short_name" {default = "gordey"}
+
 variable "az_count" {
   description = "Number of AZs to cover in a given AWS region"
   default     = "2"
