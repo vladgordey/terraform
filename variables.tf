0215a03ec6954d4b256867a268801a7ae1d0ff62
vladgordey <vladgordey@gmail.com>
2017-02-11 20:44:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,6 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "eu-central-1"
 }
-variable "short_name" {default = "gordey"}
-
 variable "vpc_cidr" {default = "10.0.0.0/16"}
 variable "private_subnet_cidr" {default = "10.0.2.0/24"}
 variable "short_name" {default = "gordey"}
